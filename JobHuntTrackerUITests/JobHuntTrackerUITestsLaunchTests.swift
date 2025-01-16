67a3c94503d3b828e00708b29fca35df5929fe1e
Titus Logo <titus.logo@gmail.com>
2025-01-16 13:23:56 +0700

    Initial Commit

---
+++ JobHuntTrackerUITests/JobHuntTrackerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  JobHuntTrackerUITestsLaunchTests.swift
+//  JobHuntTrackerUITests
+//
+//  Created by Titus Logo on 16/01/25.
+//
+
+import XCTest
+
+final class JobHuntTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
